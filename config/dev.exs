dee8b227e9c58957e6113b56534749e7d0ea2a9e
Maksim Sharov <maxsharoff@gmail.com>
2024-04-06 23:50:01 +0000

    feat: add home page

--- config/dev.exs
+++ config/dev.exs
@@ -57,7 +57,7 @@ config :runa, RunaWeb.Endpoint,
     patterns: [
       ~r"priv/static/(?!uploads/).*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
-      ~r"lib/application/(controllers|live|components)/.*(ex|heex)$"
+      ~r"lib/application/.*(ex|heex)$"
     ]
   ]
 
