c3f4a5920592e72732515f0ec0fca9062ab74f33
Maksim Sharov <maxsharoff@gmail.com>
2024-04-29 18:34:00 +0000

    feat: add input

---
+++ lib/application/components/label.ex
@@ -0,0 +1,18 @@
+defmodule RunaWeb.Components.Label do
+  @moduledoc """
+  Renders a label.
+  """
+
+  use Phoenix.Component
+
+  attr :for, :string, default: nil
+  slot :inner_block, required: true
+
+  def label(assigns) do
+    ~H"""
+    <label for={@for} class="block text-sm font-semibold leading-6 text-zinc-800">
+      <%= render_slot(@inner_block) %>
+    </label>
+    """
+  end
+end
