8f6bd88b9302e33f2b7d7d28560fb3e969a236d3
Maksim Sharov <maxsharoff@gmail.com>
2024-12-10 01:31:52 +0100

    refactor: make isolated tokens view

--- lib/application/live/profile/profile.ex
+++ lib/application/live/profile/profile.ex
@@ -5,17 +5,10 @@ defmodule RunaWeb.Live.Profile do
   use RunaWeb, :live_view
 
   alias Runa.Accounts
-  alias Runa.Tokens
-  alias Runa.Tokens.Token
 
   import RunaWeb.Components.Tab
   import RunaWeb.Components.Info
   import RunaWeb.Components.Avatar
-  import RunaWeb.Components.Button
-  import RunaWeb.Components.Icon
-  import RunaWeb.Components.Table
-  import RunaWeb.Components.Modal
-  import RunaWeb.Formatters
 
   @impl true
   def mount(_params, %{"user_id" => user_id}, socket) do
@@ -26,15 +19,9 @@ defmodule RunaWeb.Live.Profile do
     case Accounts.get(user_id) do
       {:ok, user} ->
         socket =
-          socket
-          |> assign(:is_visible_delete_token_modal, false)
-          |> assign(:is_visible_token_modal, false)
-          |> assign_new(:access, fn ->
-            Enum.map(Token.access_levels(), fn {label, _} -> {label, label} end)
-          end)
-          |> assign(token: %Token{})
-          |> assign(user: user)
-          |> stream(:tokens, user.tokens)
+          assign(socket,
+            user: user
+          )
 
         {:ok, socket}
 
@@ -61,105 +48,8 @@ defmodule RunaWeb.Live.Profile do
   end
 
   @impl true
-  def handle_info({:created_token, data}, socket) do
-    socket =
-      socket
-      |> stream_insert(:tokens, data)
-      |> assign(:token, %Token{})
-      |> assign(:is_visible_token_modal, false)
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({:updated_token, data}, socket) do
-    socket =
-      socket
-      |> stream_delete(:tokens, data)
-      |> stream_insert(:tokens, data)
-      |> assign(:token, %Token{})
-      |> assign(:is_visible_token_modal, false)
-
-    {:noreply, socket}
-  end
-
-  @impl true
   def handle_info(_, socket) do
     {:noreply, socket}
-  end
-
-  @impl true
-  def handle_event("open_delete_token_modal", %{"id" => id}, socket) do
-    case Tokens.get(id) do
-      {:ok, data} ->
-        socket =
-          socket
-          |> assign(:token, data)
-          |> assign(:is_visible_delete_token_modal, true)
-
-        {:noreply, socket}
-
-      {:error, _} ->
-        {:noreply, put_flash(socket, :error, "Failed to delete token")}
-    end
-  end
-
-  @impl true
-  def handle_event("close_delete_token_modal", _, socket) do
-    socket =
-      socket
-      |> assign(:token, %Token{})
-      |> assign(:is_visible_delete_token_modal, false)
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_event("open_token_modal", %{"id" => id}, socket) do
-    case Tokens.get(id) do
-      {:ok, token} ->
-        socket =
-          socket
-          |> assign(:token, token)
-          |> assign(:is_visible_token_modal, true)
-
-        {:noreply, socket}
-
-      _ ->
-        {:noreply, socket}
-    end
-  end
-
-  @impl true
-  def handle_event("open_token_modal", _, socket) do
-    {:noreply, assign(socket, :is_visible_token_modal, true)}
-  end
-
-  @impl true
-  def handle_event("close_token_modal", _, socket) do
-    socket =
-      socket
-      |> assign(:token, %Token{})
-      |> assign(:is_visible_token_modal, false)
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_event("delete_token", _, socket) do
-    case Tokens.delete(socket.assigns.token.id) do
-      {:ok, data} ->
-        socket =
-          socket
-          |> stream_delete(:tokens, data)
-          |> assign(:token, %Token{})
-          |> assign(:is_visible_delete_token_modal, false)
-
-        {:noreply, socket}
-
-      {:error, _} ->
-        {:noreply, put_flash(socket, :error, "Failed to delete token")}
-    end
   end
 
   @impl true
---
+++ test/application/live/token/index_test.exs
@@ -0,0 +1,372 @@
+defmodule RunaWeb.Live.Token.IndexTest do
+  @moduledoc false
+
+  use RunaWeb.ConnCase, async: true
+
+  import RunaWeb.Formatters
+
+  @moduletag :tokens
+
+  setup do
+    user = insert(:user)
+
+    {:ok, user: user}
+  end
+
+  describe "Tokens view" do
+    test "renders tokens titles", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      html =
+        parent_view
+        |> find_live_child("api_keys")
+        |> render()
+
+      for {token, index} <- Enum.with_index(ctx.user.tokens) do
+        title =
+          html
+          |> Floki.find("table[aria-label='API keys'] tbody tr")
+          |> Enum.at(index)
+          |> Floki.find("td")
+          |> Enum.at(0)
+          |> Floki.text()
+          |> String.trim()
+
+        assert title == token.title
+      end
+    end
+
+    test "renders token owner", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      html =
+        parent_view
+        |> find_live_child("api_keys")
+        |> render()
+
+      owner =
+        html
+        |> Floki.find("table[aria-label='API keys'] tbody tr")
+        |> Enum.at(0)
+        |> Floki.find("td")
+        |> Enum.at(1)
+        |> Floki.text()
+        |> String.trim()
+
+      assert owner =~ ctx.user.name
+    end
+
+    test "renders token created at", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      html =
+        parent_view
+        |> find_live_child("api_keys")
+        |> render()
+
+      for {token, index} <- Enum.with_index(ctx.user.tokens) do
+        created_at =
+          html
+          |> Floki.find("table[aria-label='API keys'] tbody tr")
+          |> Enum.at(index)
+          |> Floki.find("td")
+          |> Enum.at(2)
+          |> Floki.text()
+          |> String.trim()
+
+        assert created_at == "#{format_datetime_to_view(token.inserted_at)}"
+      end
+    end
+
+    test "renders token status", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      html =
+        parent_view
+        |> find_live_child("api_keys")
+        |> render()
+
+      for {token, index} <- Enum.with_index(ctx.user.tokens) do
+        access =
+          html
+          |> Floki.find("table[aria-label='API keys'] tbody tr")
+          |> Enum.at(index)
+          |> Floki.find("td")
+          |> Enum.at(4)
+          |> Floki.text()
+          |> String.trim()
+
+        assert access == "#{token.access}"
+      end
+    end
+
+    test "renders token delete button", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      child_view =
+        find_live_child(
+          parent_view,
+          "api_keys"
+        )
+
+      for token <- ctx.user.tokens do
+        assert has_element?(
+                 child_view,
+                 "button[aria-label='Delete token'][data-token-id='#{token.id}']"
+               )
+      end
+    end
+
+    test "renders token update button", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      child_view =
+        find_live_child(
+          parent_view,
+          "api_keys"
+        )
+
+      for token <- ctx.user.tokens do
+        assert has_element?(
+                 child_view,
+                 "button[aria-label='Update token'][data-token-id='#{token.id}']"
+               )
+      end
+    end
+  end
+
+  describe "delete token modal" do
+    test "renders by clicking delete token button", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      child_view =
+        find_live_child(
+          parent_view,
+          "api_keys"
+        )
+
+      refute has_element?(child_view, "#modal")
+
+      for token <- ctx.user.tokens do
+        child_view
+        |> element(
+          "button[aria-label='Delete token'][data-token-id='#{token.id}']"
+        )
+        |> render_click()
+
+        assert has_element?(child_view, "#modal")
+      end
+    end
+
+    test "deletes token by clicking delete button", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      child_view =
+        find_live_child(
+          parent_view,
+          "api_keys"
+        )
+
+      for token <- ctx.user.tokens do
+        assert has_element?(child_view, "td", token.title)
+
+        child_view
+        |> element(
+          "button[aria-label='Delete token'][data-token-id='#{token.id}']"
+        )
+        |> render_click()
+
+        child_view
+        |> element("button", "Delete")
+        |> render_click()
+
+        refute has_element?(child_view, "td", token.title)
+      end
+    end
+  end
+
+  describe "create token modal" do
+    test "rendered by clicking create token button", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      child_view =
+        find_live_child(
+          parent_view,
+          "api_keys"
+        )
+
+      refute has_element?(child_view, "#modal")
+
+      child_view
+      |> element("button[aria-label='Create token']")
+      |> render_click()
+
+      assert has_element?(child_view, "#modal")
+    end
+
+    test "creates token by clicking create button", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      child_view =
+        find_live_child(
+          parent_view,
+          "api_keys"
+        )
+
+      title = Atom.to_string(ctx.test)
+
+      child_view
+      |> element("button[aria-label='Create token']")
+      |> render_click()
+
+      child_view
+      |> element("form[aria-label='Token form']")
+      |> render_submit(%{
+        "token" => %{"access" => "read", "title" => title}
+      })
+
+      refute has_element?(child_view, "p", "can't be blank")
+      refute has_element?(child_view, "p", "is invalid")
+
+      assert has_element?(child_view, "td", title)
+    end
+
+    test "renders error when title is blank", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      child_view =
+        find_live_child(
+          parent_view,
+          "api_keys"
+        )
+
+      child_view
+      |> element("button[aria-label='Create token']")
+      |> render_click()
+
+      child_view
+      |> element("form[aria-label='Token form']")
+      |> render_submit(%{"token" => %{"access" => "read", "title" => ""}})
+
+      assert has_element?(child_view, "p", "can't be blank")
+    end
+
+    test "renders error when access is invalid", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      child_view =
+        find_live_child(
+          parent_view,
+          "api_keys"
+        )
+
+      title = Atom.to_string(ctx.test)
+
+      child_view
+      |> element("button[aria-label='Create token']")
+      |> render_click()
+
+      child_view
+      |> element("form[aria-label='Token form']")
+      |> render_submit(%{"token" => %{"access" => "invalid", "title" => title}})
+
+      assert has_element?(child_view, "p", "is invalid")
+    end
+  end
+
+  describe "update token modal" do
+    test "renders by clicking update token button", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      child_view =
+        find_live_child(
+          parent_view,
+          "api_keys"
+        )
+
+      refute has_element?(child_view, "#modal")
+
+      for token <- ctx.user.tokens do
+        child_view
+        |> element(
+          "button[aria-label='Update token'][data-token-id='#{token.id}']"
+        )
+        |> render_click()
+
+        assert has_element?(child_view, "#modal")
+      end
+    end
+
+    test "updates token by clicking update button", ctx do
+      {:ok, parent_view, _} =
+        ctx.conn
+        |> put_session(:user_id, ctx.user.id)
+        |> live(~p"/profile")
+
+      child_view =
+        find_live_child(
+          parent_view,
+          "api_keys"
+        )
+
+      title = Atom.to_string(ctx.test)
+
+      for token <- ctx.user.tokens do
+        assert has_element?(child_view, "td", token.title)
+
+        child_view
+        |> element(
+          "button[aria-label='Update token'][data-token-id='#{token.id}']"
+        )
+        |> render_click()
+
+        child_view
+        |> element("form[aria-label='Token form']")
+        |> render_submit(%{"token" => %{"access" => "read", "title" => title}})
+
+        refute has_element?(child_view, "td", token.title)
+
+        assert has_element?(child_view, "td", title)
+      end
+    end
+  end
+end
