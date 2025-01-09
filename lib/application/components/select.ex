1d60522459327cecd02d537d7a1554b68ad4d7eb
Maksim Sharov <maxsharoff@gmail.com>
2025-01-09 14:16:42 +0100

    refactor: add single select component

--- lib/application/components/select.ex
+++ lib/application/components/select.ex
@@ -66,7 +66,7 @@ defmodule RunaWeb.Components.Select do
       phx-click={toggle_options(@id)}
       phx-click-away={hide_options(@id)}
     >
-      Languages
+      {render_slot(@label)}
       <div
         role="combobox"
         aria-controls={@id}
@@ -95,22 +95,20 @@ defmodule RunaWeb.Components.Select do
             {render_slot(@selected, @value)}
           <% end %>
         </div>
-        <div class="flex items-center gap-1 flex-shrink-0">
-          <.icon
-            icon="x-mark"
-            class="cursor-pointer"
-            aria-label="Clear selection"
-            role="button"
-            phx-target={@target}
-            phx-click="clear_selection"
-          />
-          <.icon
-            icon="shevron-right"
-            aria-label="Toggle options"
-            role="button"
-            class="cursor-pointer rotate-90 transition-transform duration-300 group-open:rotate-[270deg]"
-          />
-        </div>
+        <.icon
+          icon="x-mark"
+          class="cursor-pointer"
+          aria-label="Clear selection"
+          role="button"
+          phx-target={@target}
+          phx-click="clear_selection"
+        />
+        <.icon
+          icon="shevron-right"
+          aria-label="Toggle options"
+          role="button"
+          class="cursor-pointer rotate-90 transition-transform duration-300 group-open:rotate-[270deg]"
+        />
       </div>
       <select
         id={@id}
@@ -128,6 +126,71 @@ defmodule RunaWeb.Components.Select do
           ])
         }
         multiple={@multiple}
+        {@rest}
+      >
+        <%= if Enum.empty?(@options) do %>
+          <option disabled>No options available</option>
+        <% else %>
+          {Phoenix.HTML.Form.options_for_select(@options, @value)}
+        <% end %>
+      </select>
+      <.error :for={msg <- @errors}>{msg}</.error>
+    </.label>
+    """
+  end
+
+  def select(assigns) do
+    ~H"""
+    <.label
+      class="relative gap-2"
+      phx-feedback-for={@name}
+      phx-click={toggle_options(@id)}
+      phx-click-away={hide_options(@id)}
+    >
+      {render_slot(@label)}
+      <div
+        role="combobox"
+        aria-controls={@id}
+        aria-orientation="vertical"
+        class="w-full border rounded focus:ring-0 sm:text-sm sm:leading-6 h-10 px-3 py-2 flex justify-between items-center gap-1"
+      >
+        <%= unless @selected != [] do %>
+          <span aria-label="Selected option" class="truncate flex-grow">
+            {@value || "Select option"}
+          </span>
+        <% else %>
+          {render_slot(@selected, @value)}
+        <% end %>
+        <.icon
+          icon="x-mark"
+          class="cursor-pointer"
+          aria-label="Clear selection"
+          role="button"
+          phx-target={@target}
+          phx-click="clear_selection"
+        />
+        <.icon
+          icon="shevron-right"
+          aria-label="Toggle options"
+          role="button"
+          class="cursor-pointer rotate-90 transition-transform duration-300 group-open:rotate-[270deg]"
+        />
+      </div>
+      <select
+        id={@id}
+        name={@name}
+        aria-hidden="true"
+        role="listbox"
+        phx-hook="infiniteScroll"
+        phx-target={@target}
+        size={max(1, length(@options))}
+        class={
+          classes([
+            "hidden border top-full absolute mt-1 p-0 rounded bg-background w-full dark:bg-background h-fit max-h-[18.75rem] overflow-auto z-50 shadow-sm transition hover:shadow focus:ring-0 sm:text-sm",
+            "[&>option]:flex [&>option]:items-center [&>option]:p-2 [&>option]:gap-[.25rem] [&>option]:truncate [&>option]:cursor-pointer [&>option:hover]:bg-background-hover",
+            @class
+          ])
+        }
         {@rest}
       >
         <%= if Enum.empty?(@options) do %>
