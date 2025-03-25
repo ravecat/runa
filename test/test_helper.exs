f81ef75a97bcdaeeb23a84f5a9540435dfe64ac9
Maksim Sharov <maxsharoff@gmail.com>
2025-03-25 13:09:33 +0100

    test: add e2e test environment
    META: story/136, bug/12

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,10 +1,11 @@
-ExUnit.configure(exclude: :skip, async: true)
+Application.put_env(:wallaby, :base_url, RunaWeb.Endpoint.url)
 
 {:ok, _} = Application.ensure_all_started(:ex_machina)
+{:ok, _} = Application.ensure_all_started(:wallaby)
 
-ExUnit.start()
-
+ExUnit.configure(exclude: [:skip], timeout: 10_000)
 Ecto.Adapters.SQL.Sandbox.mode(Runa.Repo, :manual)
 
+ExUnit.start()
 Repatch.setup()
 Faker.start()
