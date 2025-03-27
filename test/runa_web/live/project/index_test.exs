047f3db1fc3180a102dd0ebc79fc244171e6b524
Maksim Sharov <maxsharoff@gmail.com>
2025-03-27 20:25:39 +0100

    style: update projects page view
    META: story/137

--- test/runa_web/live/project/index_test.exs
+++ test/runa_web/live/project/index_test.exs
@@ -1,274 +1,234 @@
 defmodule RunaWeb.Live.Project.IndexTest do
-  use RunaWeb.ConnCase, async: true
+  use RunaWeb.FeatureCase, auth: true
 
   @moduletag :projects
 
   setup ctx do
     team = insert(:team)
     contributor = insert(:contributor, team: team, user: ctx.user)
-    projects = insert_list(2, :project, team: team)
-    file = insert(:file, project: Enum.at(projects, 0))
     language = insert(:language, wals_code: "eng", title: "English")
+    projects = insert_list(2, :project, team: team, base_language: language)
 
     {:ok,
      projects: projects,
      team: team,
      contributor: contributor,
-     localization_file: file,
      language: language}
   end
 
   describe "projects view" do
-    test "renders projects cards", ctx do
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
-
-      assert Enum.all?(
-               ctx.projects,
-               &has_element?(view, "[aria-label='Project #{&1.name} card']")
-             )
-    end
-
-    test "renders edit button", ctx do
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
-
-      assert Enum.all?(
-               ctx.projects,
-               &has_element?(view, "[aria-label='Edit #{&1.name} project']")
-             )
-    end
-
-    test "renders delete button", ctx do
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
+    feature "renders projects cards", ctx do
+      session = visit(ctx.session, "/projects")
 
       for project <- ctx.projects do
-        assert has_element?(
-                 view,
-                 "[aria-label='Delete #{project.name} project']"
-               )
+        assert_has(
+          session,
+          Query.css("[aria-label=\"Project #{project.name} card\"]")
+        )
       end
     end
 
-    test "renders duplicate button", ctx do
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
+    feature "renders edit button", ctx do
+      session = visit(ctx.session, "/projects")
 
       for project <- ctx.projects do
-        assert has_element?(
-                 view,
-                 "[aria-label='Duplicate #{project.name} project']"
-               )
+        assert_has(
+          session,
+          Query.css("[aria-label=\"Edit #{project.name} project\"]")
+        )
       end
     end
 
-    test "renders projects titles", ctx do
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
-
-      assert Enum.all?(ctx.projects, fn project ->
-               view
-               |> element("[aria-label='Project #{project.name} card']")
-               |> render() =~ project.name
-             end)
-    end
-
-    test "renders projects statistics", ctx do
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
+    feature "renders delete button", ctx do
+      session = visit(ctx.session, "/projects")
 
       for project <- ctx.projects do
-        assert has_element?(
-                 view,
-                 "[aria-label='Project #{project.name} statistics']"
-               )
+        assert_has(
+          session,
+          Query.css("[aria-label=\"Delete #{project.name} project\"]")
+        )
       end
     end
 
-    test "renders files count", ctx do
-      project = insert(:project, team: ctx.team)
-      files = insert_list(2, :file, project: project)
+    feature "renders duplicate button", ctx do
+      session = visit(ctx.session, "/projects")
 
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
-
-      assert view
-             |> element("[aria-label='Project #{project.name} files count']")
-             |> render() =~ "#{length(files)}"
+      for project <- ctx.projects do
+        assert_has(
+          session,
+          Query.css("[aria-label=\"Duplicate #{project.name} project\"]")
+        )
+      end
     end
 
-    test "renders languages count", ctx do
-      languages = insert_list(2, :language)
-      project = insert(:project, team: ctx.team, languages: languages)
-
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
-
-      assert view
-             |> element(
-               "[aria-label='Project #{project.name} languages count']"
-             )
-             |> render() =~ "2"
-    end
-
-    test "renders done percentage", ctx do
-      project = insert(:project, team: ctx.team)
-
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
-
-      assert view
-             |> element(
-               "[aria-label='Project #{project.name} translation progress']"
-             )
-             |> render() =~ "0%"
-    end
-
-    test "renders keys count", ctx do
-      project = insert(:project, team: ctx.team)
-      file = insert(:file, project: project)
-      keys = insert_list(2, :key, file: file)
-
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
-
-      assert view
-             |> element("[aria-label='Project #{project.name} keys count']")
-             |> render() =~ "#{Enum.count(keys)}"
-    end
-
-    test "renders project languages list", ctx do
+    feature "renders project locales", ctx do
       language = insert(:language)
-      project = insert(:project, team: ctx.team, languages: [language])
 
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
+      for project <- ctx.projects do
+        insert(:locale, project: project, language: language)
 
-      assert view
-             |> element("[aria-label='Project #{project.name} languages']")
-             |> render() =~ language.wals_code
+        ctx.session
+        |> visit("/projects")
+        |> assert_text(
+          Query.css("[aria-label=\"Project #{project.name} languages\"]"),
+          language.title || language.wals_code || language.iso_code ||
+            language.glotto_code
+        )
+        |> assert_has(
+          Query.css(
+            "[aria-label=\"Project #{project.name} languages\"] [role='listitem']",
+            count: 1
+          )
+        )
+      end
+    end
+
+    feature "renders projects statistics", ctx do
+      for project <- ctx.projects do
+        insert_list(2, :file, project: project)
+
+        insert_list(2, :locale,
+          project: project,
+          language: fn -> build(:language) end
+        )
+
+        ctx.session
+        |> visit("/projects")
+        |> find(
+          Query.css("[aria-label=\"Project #{project.name} statistics\"]")
+        )
+        |> assert_has(
+          Query.css("[aria-label=\"Project #{project.name} files count\"]",
+            text: "2"
+          )
+        )
+        |> assert_has(
+          Query.css("[aria-label=\"Project #{project.name} languages count\"]",
+            text: "2"
+          )
+        )
+        |> assert_has(
+          Query.css(
+            "[aria-label=\"Project #{project.name} translation progress\"]"
+          )
+        )
+        |> assert_has(
+          Query.css("[aria-label=\"Project #{project.name} keys count\"]")
+        )
+      end
     end
   end
 
-  describe "toolbar block" do
-    test "renders create project button", ctx do
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
-
-      assert has_element?(view, "[aria-label='Create new project']")
+  describe "projects toolbar" do
+    feature "renders create project button", ctx do
+      visit(ctx.session, "/projects")
+      |> assert_has(
+        Query.css("[aria-label=\"Create new project\"]", text: "New")
+      )
     end
   end
 
   describe "edit project modal" do
-    test "rendered by click edit button", ctx do
-      project = insert(:project, team: ctx.team, base_language: ctx.language)
+    feature "rendered by click edit button", ctx do
+      session = visit(ctx.session, "/projects")
 
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
-
-      refute has_element?(view, "[aria-modal='true'][role='dialog']")
-
-      element(view, "[aria-label='Edit #{project.name} project']")
-      |> render_click()
-
-      assert has_element?(view, "[aria-modal='true'][role='dialog']")
-      assert has_element?(view, "[aria-label='Project form']")
+      for project <- ctx.projects do
+        session
+        |> click(Query.css("[aria-label=\"Edit #{project.name} project\"]"))
+        |> assert_has(Query.css("[aria-modal='true'][role='dialog']"))
+        |> assert_has(Query.css("[aria-label=\"Project form\"]"))
+      end
     end
 
-    test "edits project by clicking save button", ctx do
-      project =
-        insert(:project,
-          team: ctx.team,
-          base_language: ctx.language,
-          languages: [ctx.language]
-        )
+    feature "edits project by clicking update button", ctx do
+      session = visit(ctx.session, "/projects")
 
-      title = Atom.to_string(ctx.test)
-
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
-
-      element(view, "[aria-label='Edit #{project.name} project']")
-      |> render_click()
-
-      element(view, "[aria-label='Project form']")
-      |> render_submit(%{
-        "project" => %{"name" => title, "base_language_id" => ctx.language.id}
-      })
-
-      assert view
-             |> element("[aria-label='Project #{title} card']")
-             |> render() =~ title
-
-      refute has_element?(view, "[aria-label='Edit #{project.name} project']")
-
-      assert_patched(view, "/projects")
+      for project <- ctx.projects do
+        session
+        |> click(Query.css("[aria-label=\"Edit #{project.name} project\"]"))
+        |> fill_in(Query.css("[aria-label=\"Project name\"]"), with: "New name")
+        |> click(Query.css("[aria-label=\"Confirm update project\"]"))
+        |> assert_has(Query.css("[aria-label=\"Project form\"]", count: 0))
+      end
     end
   end
 
-  describe "delete project action" do
-    test "renders delete project dialog", ctx do
-      project = List.first(ctx.projects)
+  # describe "delete project action" do
+  #   test "renders delete project dialog", ctx do
+  #     project = List.first(ctx.projects)
 
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
+  #     {:ok, view, _} = live(ctx.conn, ~p"/projects")
 
-      element(view, "[aria-label='Delete #{project.name} project']")
-      |> render_click()
+  #     element(view, "[aria-label='Delete #{project.name} project']")
+  #     |> render_click()
 
-      assert has_element?(view, "[aria-modal='true'][role='dialog']")
-    end
+  #     assert has_element?(view, "[aria-modal='true'][role='dialog']")
+  #   end
 
-    test "deletes project by clicking delete button", ctx do
-      project = List.first(ctx.projects)
+  #   test "deletes project by clicking delete button", ctx do
+  #     project = List.first(ctx.projects)
 
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
+  #     {:ok, view, _} = live(ctx.conn, ~p"/projects")
 
-      element(view, "[aria-label='Delete #{project.name} project']")
-      |> render_click()
+  #     element(view, "[aria-label='Delete #{project.name} project']")
+  #     |> render_click()
 
-      element(view, "[aria-label='Confirm delete project']")
-      |> render_click()
+  #     element(view, "[aria-label='Confirm delete project']")
+  #     |> render_click()
 
-      refute has_element?(view, "[aria-label='Project #{project.name} card']")
-    end
-  end
+  #     refute has_element?(view, "[aria-label='Project #{project.name} card']")
+  #   end
+  # end
 
-  describe "create project modal" do
-    test "rendered by click create button", ctx do
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
+  # describe "create project modal" do
+  #   test "rendered by click create button", ctx do
+  #     {:ok, view, _} = live(ctx.conn, ~p"/projects")
 
-      refute has_element?(view, "[aria-modal='true'][role='dialog']")
+  #     refute has_element?(view, "[aria-modal='true'][role='dialog']")
 
-      element(view, "[aria-label='Create new project']")
-      |> render_click()
+  #     element(view, "[aria-label='Create new project']")
+  #     |> render_click()
 
-      assert has_element?(view, "[aria-modal='true'][role='dialog']")
-      assert has_element?(view, "[aria-label='Project form']")
-    end
+  #     assert has_element?(view, "[aria-modal='true'][role='dialog']")
+  #     assert has_element?(view, "[aria-label='Project form']")
+  #   end
 
-    test "creates project by clicking create button", ctx do
-      title = Atom.to_string(ctx.test)
+  #   test "creates project by clicking create button", ctx do
+  #     title = Atom.to_string(ctx.test)
 
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
+  #     {:ok, view, _} = live(ctx.conn, ~p"/projects")
 
-      refute has_element?(view, "[aria-label='Project #{title} card']")
+  #     refute has_element?(view, "[aria-label='Project #{title} card']")
 
-      element(view, "[aria-label='Create new project']")
-      |> render_click()
+  #     element(view, "[aria-label='Create new project']")
+  #     |> render_click()
 
-      element(view, "[aria-label='Project form']")
-      |> render_submit(%{
-        "project" => %{"name" => title, "base_language_id" => ctx.language.id}
-      })
+  #     element(view, "[aria-label='Project form']")
+  #     |> render_submit(%{
+  #       "project" => %{"name" => title, "base_language_id" => ctx.language.id}
+  #     })
 
-      assert view
-             |> element("[aria-label='Project #{title} card']")
-             |> render() =~ title
+  #     assert view
+  #            |> element("[aria-label='Project #{title} card']")
+  #            |> render() =~ title
 
-      refute has_element?(view, "[aria-modal='true'][role='dialog']")
+  #     refute has_element?(view, "[aria-modal='true'][role='dialog']")
 
-      assert_patched(view, "/projects")
-    end
-  end
+  #     assert_patched(view, "/projects")
+  #   end
+  # end
 
-  describe "duplicated project" do
-    test "created by clicking duplicate button", ctx do
-      project = List.first(ctx.projects)
+  # describe "duplicated project" do
+  #   test "created by clicking duplicate button", ctx do
+  #     project = List.first(ctx.projects)
 
-      {:ok, view, _} = live(ctx.conn, ~p"/projects")
+  #     {:ok, view, _} = live(ctx.conn, ~p"/projects")
 
-      element(view, "button[aria-label='Duplicate #{project.name} project']")
-      |> render_click()
+  #     element(view, "button[aria-label='Duplicate #{project.name} project']")
+  #     |> render_click()
 
-      assert element(view, "[aria-label='Project #{project.name} (copy) card']")
-             |> render() =~ "#{project.name} (copy)"
-    end
-  end
+  #     assert element(view, "[aria-label='Project #{project.name} (copy) card']")
+  #            |> render() =~ "#{project.name} (copy)"
+  #   end
+  # end
 end
