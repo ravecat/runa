7d586c7efa4d781af42acbacdf0490410954f2f4
Maksim Sharov <maxsharoff@gmail.com>
2025-04-03 15:39:46 +0200

    refactor: transfer error translations to separate adapter

--- lib/runa_web/components/input.ex
+++ lib/runa_web/components/input.ex
@@ -28,6 +28,8 @@ defmodule RunaWeb.Components.Input do
   import RunaWeb.Components.Label
   import RunaWeb.Components.Icon
 
+  import RunaWeb.Adapters.Error
+
   attr :id, :any, default: nil
   attr :name, :any
   attr :value, :any
@@ -163,27 +165,6 @@ defmodule RunaWeb.Components.Input do
       <.error :for={msg <- @errors}>{msg}</.error>
     </div>
     """
-  end
-
-  @doc """
-  Translates an error message using gettext.
-  """
-  def translate_error({msg, opts}) do
-    # When using gettext, we typically pass the strings we want
-    # to translate as a static argument:
-    #
-    #     # Translate the number of files with plural rules
-    #     dngettext("errors", "1 file", "%{count} files", count)
-    #
-    # However the error messages in our forms and APIs are generated
-    # dynamically, so we need to translate them by calling Gettext
-    # with our gettext backend as first argument. Translations are
-    # available in the errors.po file (as we use the "errors" domain).
-    if count = opts[:count] do
-      Gettext.dngettext(RunaWeb.Gettext, "errors", msg, msg, count, opts)
-    else
-      Gettext.dgettext(RunaWeb.Gettext, "errors", msg, opts)
-    end
   end
 
   @doc """
