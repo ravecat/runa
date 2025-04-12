647cab649e377d28674aceb3861a1597558313fa
Maksim Sharov <maxsharoff@gmail.com>
2025-04-12 02:37:58 +0200

    view: add dialog compoment

---
+++ assets/svelte/ui/modals.svelte
@@ -0,0 +1,11 @@
+<script module lang="ts">
+  import { Modals } from "svelte-modals";
+  import type { ModalProps as DefaultModalProps } from "svelte-modals";
+
+  export type ModalProps<
+    Out extends Record<string, unknown> = {},
+    ReturnValue = void,
+  > = DefaultModalProps<ReturnValue> & Out;
+</script>
+
+<Modals />
